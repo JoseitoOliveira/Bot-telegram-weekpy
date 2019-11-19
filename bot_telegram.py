cf8a22e8b43b6ea1e70b510b0d4bd39186c65271
Joseíto de Oliveira Júnior <45806413+JoseitoOliveira@users.noreply.github.com>
2019-11-18 23:58:22 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -7,12 +7,16 @@ import random
 import telepot
 import yagmail
 from telepot.loop import MessageLoop
+from boto.s3.connection import S3Connection
 
 from cursos import cadastrar_cursos
 
-TOKEN = 'SEU_TOKEN_AQUI'
 
-email_bot = yagmail.SMTP(input('Email: '), input('Senha: '))
+TOKEN = S3Connection(os.environ['TOKEN'], os.environ['S3_SECRET'])
+email = S3Connection(os.environ['EMAIL'], os.environ['S3_SECRET'])
+password = S3Connection(os.environ['PASS'], os.environ['S3_SECRET']) 
+
+email_bot = yagmail.SMTP(email, password)
 
 bot = telepot.Bot(TOKEN)
 
